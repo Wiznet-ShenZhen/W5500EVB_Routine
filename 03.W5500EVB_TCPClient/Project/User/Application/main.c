f63fec7de7dba352ca2f6111dbbe302011d68eaf
Leo Wang <tank19950727@gmail.com>
2022-03-31 10:12:40 +0800

    Update

--- 03.W5500EVB_TCPClient/Project/User/Application/main.c
+++ 03.W5500EVB_TCPClient/Project/User/Application/main.c
@@ -6,41 +6,41 @@ extern uint8 rxsize[];										// 引用外部变量，声明Socket接收缓存大小
 
 int main(void)
 {
-  uint8 server_ip[4]={192,168,1,102};			// 配置远程服务器IP地址
-	uint16 server_port=5000;								// 配置远程服务器端口
-	
-  /***** MCU时钟初始化 *****/							  
-	Systick_Init(72);	
-	
+	uint8 server_ip[4] = { 192,168,1,106 };			// 配置远程服务器IP地址
+	uint16 server_port = 13000;								// 配置远程服务器端口
+
+  /***** MCU时钟初始化 *****/
+	Systick_Init(72);
+
 	/***** 配置嵌套中断向量 *****/
 	NVIC_Configuration();
-	
+
 	/***** GPIO *****/
-	GPIO_Configuration();	
-	
+	GPIO_Configuration();
+
 	/***** SPI初始化 *****/
 	WIZ_SPI_Init();
-	
+
 	/***** 串口初始化 *****/
-	USART1_Init(); 		
-		
+	USART1_Init();
+
 	/***** 硬重启W5500 *****/
 	Reset_W5500();
-	
+
 	/***** W5500的IP信息初始化 *****/
 	set_default(); 														// 设置默认MAC、IP、GW、SUB、DNS
 	set_network();														// 配置初始化IP信息并打印，初始化8个Socket
-	
+
 	/***** 打开W5500的Keepalive功能 *****/
 	setkeepalive(0);
-	
-	printf("TCP Server IP: %d.%d.%d.%d \r\n",server_ip[0],server_ip[1],server_ip[2],server_ip[3]);
-  printf("TCP Server Port: %d \r\n",server_port);	
-	printf("W5500 Init Complete!\r\n");
-  printf("Start TCP Client Test!\r\n"); 
 
-	while(1)																				
-	{	
+	printf("TCP Server IP: %d.%d.%d.%d \r\n", server_ip[0], server_ip[1], server_ip[2], server_ip[3]);
+	printf("TCP Server Port: %d \r\n", server_port);
+	printf("W5500 Init Complete!\r\n");
+	printf("Start TCP Client Test!\r\n");
+
+	while (1)
+	{
 		/***** Socket状态机 *****/
 		tcpc(server_ip);
 	}
--- 29.W5500EVB_multcast/Project/User/Application/main.c
+++ 29.W5500EVB_multcast/Project/User/Application/main.c
@@ -2,33 +2,33 @@
 
 int main()
 {
-  /***** MCU时钟初始化 *****/				  
-	Systick_Init(72);	
+	/***** MCU时钟初始化 *****/
+	Systick_Init(72);
 
 	/***** 中断控制器配置 *****/
 	NVIC_Configuration();
-	
+
 	/***** GPIO初始化 *****/
-	GPIO_Configuration();	
-	
-  /***** SPI初始化 *****/
+	GPIO_Configuration();
+
+	/***** SPI初始化 *****/
 	WIZ_SPI_Init();
-	
+
 	/***** 串口初始化 *****/
-	USART1_Init(); 			
-	
+	USART1_Init();
+
 	/***** 硬重启W5500 *****/
 	Reset_W5500();
-	
-	/***** W5500的IP信息初始化 *****/	
-	set_default(); 
-	set_network();														
-	
+
+	/***** W5500的IP信息初始化 *****/
+	set_default();
+	set_network();
+
 	printf("W5500EVB Init Complete!\r\n");
-  printf("Start Multcast Test!\r\n"); 	
-	
-	while(1)																												
+	printf("Start Multcast Test!\r\n");
+
+	while (1)
 	{
-			do_multcast();
+		do_multcast();
 	}
 }
